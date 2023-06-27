13e2d7e5429ea55135b611526d33e2de37061935
Rory MacDonald <rory@withjuno.com>
2023-06-27 20:55:13 +0100

    install cheerio, line up test to fail

--- index.test.js
+++ index.test.js
@@ -2,12 +2,14 @@ const scrapeAirBnb = require("./index.js");
 
 describe("ScrapeAirBnb:", () => {
   const mockError = "https://www.airbnb.co.uk/rooms/33571268";
-  const mockPoppyPad = "https://www.airbnb.co.uk/rooms/33571268";
+  const mockPoppyPad = "https://www.airbnb.co.uk/rooms/33571268"; // expired
   const mockNorfolk = "https://www.airbnb.co.uk/rooms/50633275";
 
-  it("scrapes airbnb and returns a body", () => {
-    const data = scrapeAirBnb(mockError);
+  it("scrapes airbnb and returns a property name", async () => {
+    const {propertyName} = await scrapeAirBnb(mockNorfolk);
 
-    expect(data).toEqual({});
+    expect(propertyName).toEqual({
+      "Lovely loft on the beautiful North Norfolk Coast",
+    });
   });
 });
