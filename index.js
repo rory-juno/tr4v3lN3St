f0eb22255ba128009bf2aeaa0a801a43191c6ee3
Rory MacDonald <rory@withjuno.com>
2023-06-28 01:46:09 +0100

    propertyname, type, bedroom/bathrooms - wip Amenities modal

--- index.js
+++ index.js
@@ -1,7 +1,55 @@
-const scrapeAirBnB = (website) => {
-  console.log("website", website);
+const puppeteer = require("puppeteer");
 
-  return;
+const scrapeAirBnB = async (room) => {
+  const startURL = "https://www.airbnb.co.uk/rooms/" + room;
+  const browser = await puppeteer.launch({ headless: "new" });
+  const page = await browser.newPage();
+
+  await page.goto(startURL);
+
+  try {
+    const title = await page.waitForSelector("h1");
+    const type = await page.waitForSelector("h2");
+    const propertyStatsLine = await page.waitForSelector("ol");
+
+    const amenitiesButton = await page.waitForSelector(
+      "div[data-section-id='AMENITIES_DEFAULT'] > section > div:nth-of-type(4) > button"
+    );
+
+    // reading original page state
+    const propertyName = await title?.evaluate((el) => el.textContent);
+    const propertyTypeLine = await type?.evaluate((el) => el.textContent);
+    const propertyStats = await propertyStatsLine?.evaluate(
+      (el) => el.textContent
+    );
+
+    const propertyType = propertyTypeLine
+      .split("hosted")[0]
+      .replace("Entire", "")
+      .trim();
+    const bedrooms = propertyStats.split(" ·  · ")[1].split(" ")[0];
+    const bathrooms = propertyStats.split(" ·  · ")[3].split(" ")[0];
+
+    // interactions
+    // amenitiesButton.click();
+    // const amenitiesModalSelector = await page.waitForSelector(
+    //   'div[aria-label="What this place offers"]'
+    // );
+
+    await browser.close();
+
+    return {
+      propertyName,
+      propertyType,
+      propertyStats: { bedrooms, bathrooms },
+    };
+  } catch {
+    console.log("Property no longer listed");
+
+    return { error: "property no longer listed" };
+  } finally {
+    await browser.close();
+  }
 };
 
 module.exports = scrapeAirBnB;
--- index.test.js
+++ index.test.js
@@ -1,15 +1,46 @@
 const scrapeAirBnb = require("./index.js");
 
 describe("ScrapeAirBnb:", () => {
-  const mockError = "https://www.airbnb.co.uk/rooms/33571268";
-  const mockPoppyPad = "https://www.airbnb.co.uk/rooms/33571268"; // expired
-  const mockNorfolk = "https://www.airbnb.co.uk/rooms/50633275";
+  const expired = "33571268";
+  // const poppyRoom = "33571268"; // expired
+  const randomRoom = "16129596";
+  const norfolkRoom = "50633275";
 
-  it("scrapes airbnb and returns a property name", async () => {
-    const {propertyName} = await scrapeAirBnb(mockNorfolk);
+  jest.setTimeout(60000);
 
-    expect(propertyName).toEqual({
-      "Lovely loft on the beautiful North Norfolk Coast",
-    });
+  it("scrapes Norfolk exampple and returns a property name and type", async () => {
+    const { propertyName, propertyType, propertyStats } = await scrapeAirBnb(
+      norfolkRoom
+    );
+
+    expect(propertyName).toEqual(
+      "Lovely loft on the beautiful North Norfolk Coast"
+    );
+
+    expect(propertyType).toEqual("guest house");
+
+    expect(propertyStats.bedrooms).toEqual("1");
+    expect(propertyStats.bathrooms).toEqual("1");
+  });
+
+  it("scrapes random example returns a property name and type", async () => {
+    const { propertyName, propertyType, propertyStats } = await scrapeAirBnb(
+      randomRoom
+    );
+
+    expect(propertyName).toEqual(
+      "Plymouth, Devon. Bovisand. Quiet, Sea Views. WIFI"
+    );
+
+    expect(propertyType).toEqual("chalet");
+
+    expect(propertyStats.bedrooms).toEqual("1");
+    expect(propertyStats.bathrooms).toEqual("1");
+  });
+
+  it("errors when property is no longer listed", async () => {
+    const { error } = await scrapeAirBnb(expired);
+
+    expect(error).toEqual("property no longer listed");
   });
 });
